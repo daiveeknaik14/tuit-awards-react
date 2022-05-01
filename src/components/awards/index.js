3f6cc0e310a26aa663421a248b088cfc58e9e0e1
Vaishnavishah <shahvaishnavi2411@gmail.com>
2022-05-01 17:01:19 -0400

    Added CSS and images for awards and Modal Component

--- src/components/awards/index.js
+++ src/components/awards/index.js
@@ -7,35 +7,78 @@ import Award from "./award";
 import React, {useEffect, useState} from "react";
 
 const Awards = ({awards = [], refreshawards, awardTuit, tuit, award}) => {
+    const headingStyle = {
+            float: 'center',
+            fontSize: '50px',
+    fontWeight: '600',
+    backgroundImage: 'linear-gradient(to left, #e66465, #9198e5)',
+    color: 'transparent',
+    backgroundClip: 'text',
+    webkitBackgroundClip: 'text',
+
+    };
     return (
         <div>
-            <ul className="ttr-tuits list-group">
+            <h1 style={headingStyle}>Awards!</h1>
+            <ul className="ttr-tuits list-group" style={{
+                display: "grid",
+                gridTemplateColumns: "1fr 1fr 1fr 1fr"
+            }}>
                 {
                     awards.map && awards.map(award =>
                         <Award tuit={tuit} awardTuit={awardTuit} key={award._id}
-                              award={award} award={award} / >)
+                              award={award} award={award} />)
                 }
 
             </ul>
             <hr/>
-            <ul>
+            <h1 style={headingStyle}>Awards Received!</h1>
+            <ul style={{
+                display: "grid",
+                gridTemplateColumns: "1fr 1fr 1fr 1fr"
+            }}>
+
             {
                 award && <>
                 {award.A1 && <>
-                    <div>A1: {award.A1}</div>
+                    <img src={`../images/A1.png`}/>
+                    <h1>{award.A1}</h1>
                 </>
                 }
                 {award.A2 && <>
-                    <div>A2: {award.A2}</div>
+                    <img src={`../images/A2.png`}/>
+                    <h1>{award.A2}</h1>
                 </>
                 }
                 {award.A3 && <>
-                    <div>A3: {award.A3}</div>
+                    <img src={`../images/A3.png`}/>
+                    <h1>{award.A3}</h1>
                 </>
                 }
                 {award.A4 && <>
-                    <div>A4: {award.A4}</div>
+                    <img src={`../images/A4.png`}/>
+                    <h1>{award.A4}</h1>
                 </>
+                }
+                {award.A5 && <>
+                    <img src={`../images/A5.png`}/>
+                    <h1>{award.A5}</h1>
+                </>
+                }
+                {award.A6 && <>
+                    <img src={`../images/A6.png`}/>
+                    <h1>{award.A6}</h1>
+                </>
+                }
+                {award.A7 && <>
+                    <img src={`../images/A7.png`}/>
+                        <h1>A7: {award.A7}</h1>
+                    </>
+                }
+                {award.A8 && <>
+                    <img src={`../images/A8.png`}/>
+                        <h1>A8: {award.A8}</h1>
+                    </>
                 }
                 </>
             }
